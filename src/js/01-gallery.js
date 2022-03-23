e5975b724b8a83ae8625128df48258843d75d6d4
Alfat1989 <gilmalf89@gmail.com>
2022-03-23 13:34:44 +0300

    добавляет библиотеки для 1 задачи

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery")
+galleryEl.style.listStyle="none"
+
+function addGalleryMarkup(gallery) {
+    return gallery.map(({original, preview, description}) => 
+        `
+        <li>
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            alt="${description}"/>
+            </a>
+        </li>
+        `
+    ).join('')
+}
+
+galleryEl.innerHTML = addGalleryMarkup(galleryItems)
+
+
+const instance = new SimpleLightbox('.gallery a', {
+        
+            animationSpeed: 250,
+            loop: true,
+            enableKeyboard: true,
+            preloading: true,
+        docClose: true,
+        captionsData: 'alt',
+        }
+    );
+
+console.log('hello')
