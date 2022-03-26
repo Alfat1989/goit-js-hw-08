0d26f120542d427c8383099826fcf4fbbece3602
Alfat1989 <gilmalf89@gmail.com>
2022-03-26 22:53:56 +0300

    вешает событие на инпуты в форме

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,20 @@
-
+import throttle from "lodash.throttle"
 
 const iframe = document.querySelector('iframe')
 console.log(iframe)
+
+const player = new Vimeo.Player(iframe);
+const PLAY_KEY="videoplayer-current-time"
+player.on('timeupdate', throttle(onPlayTimer, 1000));
+
+function onPlayTimer(data) {
+    localStorage.setItem(PLAY_KEY, JSON.stringify(data))
+    console.log(data.seconds)
+}
+
+const savedTime = localStorage.getItem(PLAY_KEY)
+const parseTime = JSON.parse(savedTime)
+console.log(parseTime)
+
+player.setCurrentTime(parseTime.seconds)
+ 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from "lodash.throttle"
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', onSubmit)
+form.addEventListener('input', throttle(onInputForm, 500))
+
+populateValue()
+
+function onInputForm(e) {
+    //записывает значения в локальное хранилище
+    localStorage.setItem(e.target.name, e.target.value)
+    
+}
+
+function populateValue() {
+    // берет значения из локального хранилища если они там есть
+    //  и прописывает их в инпуты после перезагрузки
+    const emailVal = localStorage.getItem('email')
+    const messagelVal = localStorage.getItem('message')
+    if (emailVal||messagelVal) {
+        form.elements.email.value=emailVal
+        form.elements.message.value=messagelVal
+    }
+
+}
+
+
+function onSubmit(e) {
+    e.preventDefault()
+
+    const formObj = {}
+    // создает объект и записывает в них значения полей которые были при отправке формы
+    formObj.email = form.elements.email.value;
+    formObj.mesaage = form.elements.message.value;
+
+    // Удаляет значения хранилища и инпутов после отправки формы 
+    e.currentTarget.reset()
+    localStorage.removeItem('email')
+    localStorage.removeItem('message')
+
+    // Если форма пустая то не консолит объект
+    if (formObj.email!=='' && formObj.mesaage!=='') {
+        console.log(formObj)
+    } 
+    
+}
+
 
