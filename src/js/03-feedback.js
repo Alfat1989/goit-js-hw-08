7957f6d03453613f7fff965abda707eeb024a5cd
Alfat1989 <gilmalf89@gmail.com>
2022-03-29 11:21:10 +0300

    Добавляет обьект с данными

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,32 @@
 import throttle from "lodash.throttle"
 
 const form = document.querySelector('form')
-
 form.addEventListener('submit', onSubmit)
 form.addEventListener('input', throttle(onInputForm, 500))
 
 const formObj = {}
-const KEY_STORAGE='key-forms-storage'
+const KEY_STORAGE='feedback-form-state'
 
 
-populateValue()
+populateTextArea()
 
 function onInputForm(e) {
     //записывает значения в локальное хранилище
     formObj[e.target.name]=e.target.value
-    localStorage.setItem(KEY_STORAGE, JSON.stringify(formObj))
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(formObj))   
     
 }
 
-function populateValue() {
+function populateTextArea() {
     // берет значения из локального хранилища если они там есть
     //  и прописывает их в инпуты после перезагрузки
     const dataVal = localStorage.getItem(KEY_STORAGE)
 
-    const dataPars = JSON.parse(dataVal)
-
-    if (dataPars) {
+    if (dataVal) {
+        const dataPars = JSON.parse(dataVal)
         form.email.value=dataPars.email
-        form.message.value=dataPars.message
+        form.message.value = dataPars.message
+        
     }
 
 }
@@ -38,15 +37,12 @@ function onSubmit(e) {
     // Если форма пустая то не консолит объект
     if (form.email.value!=='' && form.message.value!=='') {
         console.log(formObj)
+
+        e.currentTarget.reset()
+        localStorage.removeItem(KEY_STORAGE)
     }
     
-    // Удаляет значения хранилища и инпутов после отправки формы 
-    // if (form.email.value==='' || form.message.value==='') {
-    //     return
-    // }
     
-    e.currentTarget.reset()
-    localStorage.removeItem(KEY_STORAGE)
 }
 
 
